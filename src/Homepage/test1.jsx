9fb92a0c74b9c912d10f3da1aec39ad399d66cea
gllod <79996338+gllod@users.noreply.github.com>
2023-06-07 17:33:11 +0300

    Test

--- src/Homepage/test1.jsx
+++ src/Homepage/test1.jsx
@@ -5,7 +5,7 @@ function test1() {
     <div>
       <h1>:24:11 PM] Failed to load jshint library
 (node:11772) UnhandledPromiseRejectionWarning: Unhandled promise rejection (rejection id: 46): Error: Failed to load jshint library. Please install jshint in your workspace folder using 'npm install jshint' or globally using 'npm install -g jshint' and then reload.
-[Error - 5:25:26 PM] Failed to load jshint library
+[Error - 5:25:26 PM] ailed to load jshint library
 (node:11772) UnhandledPromiseRejectionWarning: Unhandled promise rejection (rejection id: 47): Error: Failed to load jshint library. Please install jshint in your workspace folder using 'npm install jshint' or globally using 'npm install -g jshint' and then reload.
 </h1>
     </div>
