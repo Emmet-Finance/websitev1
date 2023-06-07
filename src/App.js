059a7dbd9d2d38cf5d2f92412aad9a4e771fac49
gllod <79996338+gllod@users.noreply.github.com>
2023-06-07 17:32:18 +0300

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './responsive.css';
 import Homepage from './Homepage/Homepage';
 import Form from './Homepage/Form';
 import React from 'react';
-import test1 from './Homepage/test1';
+import Test1 from './Homepage/Test1';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/Form" element={<Form />} />
-          <Route path="/test1" element={<test1 />} />
+          <Route path="/test1" element={<Test1 />} />
         </Routes>
       </Router>
     </div>
