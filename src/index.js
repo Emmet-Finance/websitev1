ab3a96a5e059caf2cf96d4c68de22d61fbce50b0
gllod <79996338+gllod@users.noreply.github.com>
2023-06-12 11:55:55 +0300

    google analitics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
+
+// Инициализация Google Analytics
+ReactGA.initialize('UA-265686052-1');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
