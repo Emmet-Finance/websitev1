a34710e0ec453763af5e9b4e5b6fd266738afae3
gllod <79996338+gllod@users.noreply.github.com>
2023-06-17 19:40:15 +0300

    bridge

--- src/BridgeRedirect.js
+++ src/BridgeRedirect.js
@@ -1,12 +1,10 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const BridgeRedirect = () => {
-  const navigate = useNavigate();
 
   useEffect(() => {
-    navigate('https://master.d3rd013nklfbmi.amplifyapp.com/', { replace: true });
-  }, [navigate]);
+    window.location.href = 'https://master.d3rd013nklfbmi.amplifyapp.com/';
+  }, []);
 
   return null;
 };
