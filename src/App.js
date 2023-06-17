e0d633f1845e561b7e759f3d29a8213dfa902525
gllod <79996338+gllod@users.noreply.github.com>
2023-06-17 19:49:06 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './Homepage/Homepage';
@@ -6,8 +7,37 @@ import PrivacyPolicy from './Homepage/PrivacyPolicy';
 import ScrollToTop from 'react-scroll-to-top';
 import './App.css';
 import './responsive.css';
+=======
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import './responsive.css';
+import Homepage from './Homepage/Homepage';
+import Form from './Homepage/Form';
+import PrivacyPolicy from './Homepage/PrivacyPolicy';
+import ScrollToTop from "react-scroll-to-top";
+import React from 'react';
+
+
+// Инициализация Google Analytics
+ReactGA.initialize('G-0DP30PHL61');
+
+
+function usePageViews() {
+  let location = useLocation();
+
+  useEffect(() => {
+    ReactGA.set({ page: location.pathname }); // Update the user's current page
+    ReactGA.pageview(location.pathname); // Record a pageview for the given page
+  }, [location]);
+}
+>>>>>>> parent of 33cd2c3 (no google analitic)
 
 function App() {
+  usePageViews();
+
   return (
     <div className="App">
       <Router>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
 
+// Инициализация Google Analytics
+ReactGA.initialize('G-0DP30PHL61');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
