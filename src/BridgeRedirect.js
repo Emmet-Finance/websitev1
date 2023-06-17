87770c04fa196032dbab95a7d419cd52300b644f
gllod <79996338+gllod@users.noreply.github.com>
2023-06-17 19:29:43 +0300

    BridgeRedirect.js

---
+++ src/BridgeRedirect.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+const BridgeRedirect = () => {
+  return <Redirect to="https://master.d3rd013nklfbmi.amplifyapp.com/" />;
+};
+
+export default BridgeRedirect;
