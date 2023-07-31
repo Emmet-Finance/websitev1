b85067e60b200f5d5c19c20e635438bfd2580f1c
Mithras <dimabrook79@gmail.com>
2023-07-31 21:26:17 +0300

    add: TokenItem.jsx

---
+++ src/Bridge/components/TokenItem.jsx
@@ -0,0 +1,27 @@
+import React, { useRef, useEffect, useState } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+import CopySmall from '../../assets/img/new/copy2.svg';
+import Metamask from '../../assets/img/new/Metamask.svg';
+import Star from '../../assets/img/new/star.svg';
+
+function TokenItem(props) {
+
+    return (
+        <Dropdown.Item href={props.href}>
+            <div className='flexBox'>
+                {props && props.logo && <div className='Logo' dangerouslySetInnerHTML={{ __html: props.logo }}></div>}
+                <div className='Caption'>{props.name}</div>
+            </div>
+            <p className='tokkenValue'><span>{props.name}:</span> {props.balance}</p>
+            <div className="hoverIcons">
+                <button type='button' className='copyLink'><img src={CopySmall} alt="CopySmall" /></button>
+                <button type='button' className='copyLink'><img src={Metamask} alt="Metamask" /></button>
+                <button type='button' className='copyLink'><img src={Star} alt="Star" /></button>
+            </div>
+        </Dropdown.Item>
+    )
+
+}
+
+export default TokenItem;
