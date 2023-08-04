b3b8dca74819297d595d6743d38113f7d369eeed
Mithras <dimabrook79@gmail.com>
2023-08-04 13:22:06 +0300

    refactor src/react-app-env.d.ts

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+import {EthereumProvider} from 'emmet.sdk';
+
+declare global {
+    export interface Window {
+        ethereum?: EthereumProvider;
+    }
+}
+
+export declare let window: Window & typeof globalThis;
