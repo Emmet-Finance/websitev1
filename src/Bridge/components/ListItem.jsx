cd36001d525bebb6ecb30b7a8e24d9b8da9fcc38
Mithras <dimabrook79@gmail.com>
2023-08-04 13:24:16 +0300

    fix the List key error

--- src/Bridge/components/ListItem.jsx
+++ src/Bridge/components/ListItem.jsx
@@ -4,7 +4,7 @@ import Dropdown from 'react-bootstrap/Dropdown';
 function ListItem(props) {
 
     return (<div onClick={props.onClick}>
-        <Dropdown.Item key={props.key} href={props.href}>
+        <Dropdown.Item eventKey={props.name} href={props.href}>
             {props && props.logo && <div className='Logo' dangerouslySetInnerHTML={{ __html: props.logo }}></div>}
             <div className='Caption'>{props.name}</div>
         </Dropdown.Item>
