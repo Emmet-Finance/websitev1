1baade453030149907fa0a078459a03bcd7daeea
Mithras <dimabrook79@gmail.com>
2023-08-04 13:26:09 +0300

    fix isMetamask logic

--- src/Bridge/state/wallets.ts
+++ src/Bridge/state/wallets.ts
@@ -1,30 +1,45 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { /*createAsyncThunk,*/ createSlice } from '@reduxjs/toolkit';
 
 export const walletSlice = createSlice({
-    name:'wallets',
-    initialState:{
+    name: 'wallets',
+    initialState: {
         account: '',
         accounts: [],
-        approveSuccess: false,
         balance: undefined,
+        chainId: undefined,
         isMetamask: false,
         isConnected: false,
         selectedWallet: '',
     },
-    reducers:{
-        setIsConnected: (state: any, action: { payload: any; }) => {
-            state.isConnected = action.payload;
+    reducers: {
+        setAccounts: (state: any, action: { payload: []; }) => {
+            state.accounts = action.payload;
+            state.account = state.accounts[0];
+            state.isConnected = true;
+        },
+        setBalance: (state: any, action: { payload: any; }) => {
+            state.balance = action.payload;
+        },
+        setChainId: (state: any, action: { payload: any; }) => {
+            state.chainId = action.payload;
         },
         setWallet: (state: any, action: { payload: string; }) => {
             state.selectedWallet = action.payload;
+            if(action.payload === 'Metamask'){
+                state.isMetamask = true;
+            }else{
+                state.isMetaMask = false;
+            }
         }
     },
-    extraReducers: (builder: any) => {
-        builder
-    }
+    // extraReducers: (builder: any) => {
+    //     builder
+    // }
 });
 
 export const {
-    setIsConnected,
-    setWallet
+    setAccounts,
+    setBalance,
+    setChainId,
+    setWallet,
 } = walletSlice.actions;
