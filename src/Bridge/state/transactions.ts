446b19675e9c8734ca14cf8e6b52aaf95232247c
Mithras <dimabrook79@gmail.com>
2023-08-25 21:37:34 +0300

    add: sendInstallment AsyncThunk

--- src/Bridge/state/transactions.ts
+++ src/Bridge/state/transactions.ts
@@ -1,5 +1,22 @@
-import { /*createAsyncThunk,*/ createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { addCookie } from '../utils';
+import { getTransaction, transferERC20 } from '../wallets/EVM';
+
+export const sendInstallment = createAsyncThunk('send-installment', async (params: any) => {
+    const txHash = await transferERC20(
+        params.fromChain,
+        params.toChain,
+        params.fromTokens,
+        params.transferAmount,
+        params.destinationAddress
+    );
+    const TX = await getTransaction(
+        params.fromChain,
+        `0x${txHash.slice(2)}`
+    );
+
+    return {hash:txHash, status:TX.status}
+})
 
 export const transactionSlice = createSlice({
     name: 'transaction',
@@ -7,15 +24,16 @@ export const transactionSlice = createSlice({
         approvedAmt: 0,
         approvedHash: '',
         approveSuccess: false,
-        destinationAddress: '',
+        destinationAddress: '0x738b2B2153d78Fc8E690b160a6fC919B2C88b6A4',
         destinationFee: 0,
-        destinationHash: '',
+        destinationHash: '0x930769eba119329a318ca3ada312654b0aa721df516802d9d6303e7925ff305b',
         estimationDestination: "",
         estimationNative: "",
         nativeFee: 0,
-        name:'',
-        originalHash: '',
+        originalHash: '0x74ce66640f3558a6df42be4de5621ff9ebfc38f63a7e1360a0dd11bdaf796094',
         pending: false,
+        requireApproval: false,
+        receiveAmount:0,
         slippage: 0,
         transferAmount: 0,
         transferSuccess: false,
@@ -23,6 +41,11 @@ export const transactionSlice = createSlice({
     reducers: {
         setApprovedAmount: (state: any, action) => {
             state.approvedAmt = action.payload;
+            if(state.transferAmount < state.approvedAmt){
+                state.requireApproval = true;
+            }else{
+                state.requireApproval = false;
+            }
         },
         setApprovedHash: (state: any, action) => {
             state.approvedHash = action.payload;
@@ -32,6 +55,7 @@ export const transactionSlice = createSlice({
         },
         setDestinationAccount: (state: any, action) => {
             state.destinationAddress = action.payload;
+            console.log("state.destinationAddress", state.destinationAddress)
             addCookie({
                 key: "toAddress",
                 value: action.payload,
@@ -53,6 +77,9 @@ export const transactionSlice = createSlice({
         setPending: (state: any, action) => {
             state.pending = action.payload;
         },
+        setReseiveAmount: (state: any, action) => {
+            state.receiveAmount = action.payload;
+        },
         setSlippage: (state: any, action) => {
             state.slippage = action.payload;
             addCookie({
@@ -61,19 +88,37 @@ export const transactionSlice = createSlice({
                 ...state.cookieExpires
             })
         },
-        setTransactionName: (state: any, action) => {
-            state.name = action.payload;
-        },
         setTransferAmount: (state: any, action) => {
             state.transferAmount = action.payload;
+            if(state.transferAmount < state.approvedAmt){
+                state.requireApproval = true;
+            }else{
+                state.requireApproval = false;
+            }
         },
         setTransferSuccess: (state: any, action) => {
             state.transferSuccess = action.payload;
         }
     },
-    // extraReducers: (builder: any) => {
-    //     builder
-    // }
+    extraReducers: (builder: any) => {
+        builder
+        .addCase(sendInstallment.fulfilled, (state:any, action: any) => {
+            const {hash, status} = action.payload
+            state.originalHash = hash;
+            state.transferSuccess = status;
+            state.pending = false;
+        })
+        .addCase(sendInstallment.pending, (state:any) => {
+            state.pending = true;
+            state.originalHash = '';
+            state.transferSuccess = '';
+        })
+        .addCase(sendInstallment.rejected, (state:any) => {
+            state.pending = false;
+            state.originalHash = '';
+            state.transferSuccess = '';
+        })
+    }
 });
 
 export const {
@@ -84,9 +129,10 @@ export const {
     setDestinationFee,
     setDestinationHash,
     setNativeFee,
+    setOriginalHash,
     setPending,
+    setReseiveAmount,
     setSlippage,
-    setTransactionName,
     setTransferAmount,
     setTransferSuccess,
 } = transactionSlice.actions;
