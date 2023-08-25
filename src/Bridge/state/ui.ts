ed36a0b0342e764208866eded00fd975d1fe3faf
Mithras <dimabrook79@gmail.com>
2023-08-25 21:32:16 +0300

    create: state-> ui

---
+++ src/Bridge/state/ui.ts
@@ -0,0 +1,39 @@
+import { /*createAsyncThunk,*/ createSlice } from '@reduxjs/toolkit';
+
+export const uiSlice = createSlice({
+    name: 'ui',
+    initialState:{
+        errorMessage: '',
+        isTransferReady: false,
+        isBridgeFormVisible: false,
+        isTxDetailVisible:true,
+        needApproval: false,
+    },
+    reducers: {
+        setErrorMessage: (state: any, action) => {
+            state.errorMessage = action.payload;
+        },
+        setIsTransferReady: (state: any, action) => {
+            state.isTransferReady = action.payload;
+        },
+        setIsBridgeFormVisible: (state: any) => {
+            state.isBridgeFormVisible = true;
+            state.isTxDetailVisible = false;
+        },
+        setIsTxDetailVisible: (state: any) => {
+            state.isTxDetailVisible = true;
+            state.isBridgeFormVisible = false;
+        },
+        setNeedApproval: (state: any, action) => {
+            state.needApproval = action.payload;
+        }
+    }
+});
+
+export const {
+    setErrorMessage,
+    setIsTransferReady,
+    setIsBridgeFormVisible,
+    setIsTxDetailVisible,
+    setNeedApproval,
+} = uiSlice.actions;
