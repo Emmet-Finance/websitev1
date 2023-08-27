372e9e472c838066e0116cae5b72570382cbb21b
Mithras <dimabrook79@gmail.com>
2023-08-27 10:49:25 +0300

    add: bigIntToHuman, formatFractionalPart

--- src/Bridge/utils.ts
+++ src/Bridge/utils.ts
@@ -234,15 +234,26 @@ export function shortenAddress(address: string) {
     return `${address.slice(0, 6)}…${address.slice(38, 42)}`
 }
 
+
 /**
  * Finds one chain by name
- * @param chains an array of supported chains
+ * @param chains a hashmap of supported chains
  * @param chainName the selected one
  * @returns only the selected chain
  */
-export function findChain(chains: EVMChain[], chainName: string): EVMChain {
-    return chains.filter(chain =>
-        chain.name.toLowerCase() === chainName.toLowerCase())[0];
+export function findChain<T>(
+    chains: { [key in keyof T]: EVMChain },
+    chainName: string
+): EVMChain {
+    if (chains && chainName) {
+        const cleanName = chainName
+            .toLowerCase()
+            .replace(/[^a-z]/g, '')
+        return chains[cleanName as keyof T]
+    }
+    const keys = Object.keys(chains);
+    const key = keys[0] as keyof T;
+    return chains[key];
 }
 
 /**
@@ -251,9 +262,23 @@ export function findChain(chains: EVMChain[], chainName: string): EVMChain {
  * @param chainName the selected one
  * @returns the original list minus selected
  */
-export function filterOneOut(chains: EVMChain[], chainName: string): EVMChain[] {
-    return chains.filter(chain =>
-        chain.name.toLowerCase() !== chainName.toLowerCase());
+export function filterOneOut<T>(
+    chains: { [key in keyof T]: EVMChain },
+    chainName: string
+): EVMChain[] | [] {
+
+    if (chains && chainName) {
+        const chainValues: EVMChain[] | [] = Object.values(chains)
+        try {
+            return chainValues.filter(chain =>
+                chain.name.toLowerCase() !== chainName.toLowerCase());
+        } catch (error) {
+            console.error(error)
+            return [];
+        }
+
+    }
+    return [];
 }
 
 export function format2BigInt(
@@ -302,8 +327,94 @@ export const getUintDiff = (
 
 export function chainNameToKey<T>(chainName: string): T {
 
-        return (chainName
-            .toLowerCase()
-            .replace(/[^a-zA-Z]/g, '') as unknown) as T;
+    return (chainName
+        .toLowerCase()
+        .replace(/[^a-zA-Z]/g, '') as unknown) as T;
 
+}
+
+/**
+ * Converts a bigint or string * decimals
+ * @param n an amount to be converted
+ * @param decimals places after period
+ * @returns an integer or a float as string
+ */
+export function bigIntToHuman(
+    n: string | number | bigint | undefined,
+    decimals: string | number | bigint = 18n
+): string {
+
+    if (n && decimals) {
+        let cleaned: string | number | bigint | boolean;
+        let whole, fraction: string | number | bigint;
+        let divider: bigint = typeof decimals === 'string'
+            ? 10n ** BigInt(parseInt(decimals))
+            : 10n ** BigInt(decimals);
+
+
+        switch (typeof n) {
+            case 'string':
+                cleaned = n.replace(/[^0-9.]/g, '');
+                [whole, fraction] = cleaned.split('.')
+                if (!fraction) { fraction = '' }
+                const intger = BigInt(whole + fraction) / divider;
+                fraction = BigInt(whole + fraction) % divider;
+                whole = intger;
+                break;
+            case 'number':
+                whole = parseInt(n.toString());
+                fraction = (parseFloat(n.toString()) - whole)
+                    .toString()
+                    .split('.')[1]
+                fraction = parseInt(fraction) > 0 ? parseInt(fraction) : 0;
+                break;
+            case 'bigint':
+                whole = BigInt(n) / divider;
+                fraction = BigInt(n) % divider;
+                break;
+        }
+
+        if (fraction) {
+            const formattedFractionalPart = formatFractionalPart(fraction.toString());
+            return `${whole.toLocaleString()}.${formattedFractionalPart}`
+        } else {
+            return whole.toLocaleString();
+        }
+
+    } else {
+        return ''
+    }
+}
+
+/**
+ * Removes the trailing insignificant zeros
+ * @param fraction the fractional part of a number
+ * @returns a fractional number without insignificant zeros
+ * 
+ * Example:
+ * ```ts
+ * const formatted: string = formatFractionalPart("123.4560700")
+ * ```
+ * 
+ * Returns: `123.45607`
+ */
+export function formatFractionalPart(fraction: string): string {
+    if (fraction) {
+        let lastSignificant = -1;
+        for (let i = fraction.length - 1; i >= 0; i--) {
+            if (fraction[i] !== '0') {
+                lastSignificant = i;
+                break;
+            }
+        }
+        // Find the index of the last non-zero digit in the fractional part
+        const lastNonZeroIndex = lastSignificant >= 0
+            ? lastSignificant + 1 : 0;
+        [...fraction].reverse().findIndex(char => char !== '0');
+        // Construct the formatted number with the determined significant digits
+        const formattedFractionalPart = fraction.substring(0, lastNonZeroIndex);
+        return formattedFractionalPart;
+    } else {
+        return '';
+    }
 }
