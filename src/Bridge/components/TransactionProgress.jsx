ef969be16f970a18f2dec18d7efce13d507da2f5
Mithras <dimabrook79@gmail.com>
2023-08-28 08:30:36 +0300

    animate: ProgressBar

--- src/Bridge/components/TransactionProgress.jsx
+++ src/Bridge/components/TransactionProgress.jsx
@@ -1,5 +1,5 @@
 // External imports
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAppSelector } from '../state/store';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
@@ -9,15 +9,28 @@ function TransactionProgress() {
 
     const [now, setNow] = useState(0);
 
+    useEffect(() => {
+        setInterval(() => {
+            setNow(prev => prev + 1)
+        }, 100)
+    },[]);
+
+    useEffect(() => {
+        if( now >= 100){
+            setNow(0)
+        }
+    }, [now])
+
     return (
         <>
             {transaction && transaction.pending
                 ? (<div className="approvingLoading">
-                    Approving in progress
+                    Transaction in progress:
                     <ProgressBar
+                        animated
                         striped
                         variant="success"
-                        now={100}
+                        now={now}
                         label={`${now}%`}
                         visuallyHidden
                     />
