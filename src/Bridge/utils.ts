1e8eec6b4bde1df5cd641cfece1232838c105b3d
Mithras <dimabrook79@gmail.com>
2023-09-09 20:05:43 +0300

    fix: bigIntToHuman dealing with hex string input

--- src/Bridge/utils.ts
+++ src/Bridge/utils.ts
@@ -365,6 +365,90 @@ export function chainNameToKey<T>(chainName: string): T {
 }
 
 /**
+ * Convert a hexadecimal string to a decimal string.
+ * 
+ * @param {string} s The hexadecimal string to convert.
+ * @returns {string} The decimal representation of the hexadecimal string.
+ * 
+ * @example
+ * ```js
+ * const hexadecimalString = "0x89f8e091e5e80d5";
+ * const decimalString = hexToDecimalString(hexadecimalString);
+ * console.log(`Hexadecimal: ${hexadecimalString}`);
+ * console.log(`Decimal: ${decimalString}`);
+ * // Output:
+ * // Hexadecimal: 0x89f8e091e5e80d5
+ * // Decimal: 621371443415777493
+ * ```
+ */
+function hexToDecimalString(s: string): string {
+
+    // Define a mapping from hexadecimal characters to decimal values
+    const hexToDecimalMap: Record<string, string> = {
+        '0': '0',
+        '1': '1',
+        '2': '2',
+        '3': '3',
+        '4': '4',
+        '5': '5',
+        '6': '6',
+        '7': '7',
+        '8': '8',
+        '9': '9',
+        'a': '10',
+        'b': '11',
+        'c': '12',
+        'd': '13',
+        'e': '14',
+        'f': '15',
+    };
+
+    let hexString: string;
+    // Remove the '0x' prefix if it exists
+    if (s.startsWith('0x')) { hexString = s.slice(2); }
+    else { hexString = s; }
+
+    let decimalValue = '0';
+
+    // Iterate through the hexadecimal string from right to left
+    for (let i = hexString.length - 1; i >= 0; i--) {
+        const hexDigit = hexString[i].toLowerCase();
+        const decimalDigit = hexToDecimalMap[hexDigit];
+        if (decimalDigit === undefined) {
+            throw new Error(`Invalid hexadecimal character: ${hexDigit}`);
+        }
+        const powerOf16 = BigInt(hexString.length - 1 - i);
+        const digitValue = BigInt(decimalDigit);
+        decimalValue = (BigInt(decimalValue) + digitValue * (16n ** powerOf16)).toString();
+    }
+
+    return decimalValue;
+}
+
+/**
+ * Check if a given string is a valid hexadecimal string with or without the '0x' prefix.
+ * @param s The string to check.
+ * @returns True if the string is a valid hexadecimal string, false otherwise.
+ * 
+ * @example
+ * ```ts
+ * console.log(isHexString("0x89f8e091e5e80d5")); // true
+ * console.log(isHexString("89f8e091e5e80d5"));  // true
+ * console.log(isHexString("123XYZ"));           // false
+ * ```
+ */
+function isHexString(s: any): boolean {
+    // Check if the input is a string
+    if (!s || typeof s !== 'string') return false;
+
+    // Define a regular expression pattern for a hexadecimal string
+    const hexadecimalRegex = /^0x[0-9A-Fa-f]+$/g;
+
+    // Test if the string matches the pattern
+    return hexadecimalRegex.test(s);
+}
+
+/**
  * Converts a bigint or string * decimals
  * @param n an amount to be converted
  * @param decimals places after period
@@ -384,7 +468,11 @@ export function bigIntToHuman(
 
         switch (typeof n) {
             case 'string':
-                cleaned = n.replace(/[^0-9\.]/g, '');
+                if (isHexString(n)) {
+                    cleaned = hexToDecimalString(n);
+                } else {
+                    cleaned = n.replace(/[^0-9.]/g, '');
+                }
                 [whole, fraction] = cleaned.split('.')
 
                 if (!fraction) {
