0df0261f046df346c22538f982626e0e9c91513f
Mithras <dimabrook79@gmail.com>
2023-09-16 15:35:16 +0300

    fix: safeToBigInt for floating point numbers

--- src/Bridge/wallets/safeToBigInt.ts
+++ src/Bridge/wallets/safeToBigInt.ts
@@ -5,14 +5,18 @@
  * @returns a bigint representation of the input
  */
 export function safeToBigInt(x: string | number | bigint): bigint {
-    switch(typeof x){
+    console.log("typeof x", typeof x, x)
+    switch (typeof x) {
         case 'bigint':
             return x;
         case 'number':
+            if (x.toString().includes('.')) {
+                return safeToBigInt(x.toString())
+            }
             return BigInt(x);
         case 'string':
-            if(x ){
-                return BigInt(x.replace('.', ''))
+            if (x) {
+                return BigInt(x.replace(/\./g, ''));
             }
             return 0n;
         default:
